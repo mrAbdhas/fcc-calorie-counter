b633e27318999db5a6fab4ec4d08cc30eb3b1e9b
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-01-11 19:19:15 +0100

    step1-20
    fcc practice project: Learn Form Validation by Building a Calorie Counter

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+let isError = false;
+
+//function that clean a string value and ensure we have a number
+/* because: "Even though you set an input element to be a number,
+ JavaScript receives a string value"*/
+
+ function cleanInputString(str) {
+    const strArray = str.split("");
+    const cleanStrArray = [];
+ }
